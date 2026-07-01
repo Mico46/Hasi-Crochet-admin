d68e5c22334b8b789d754a1ccd6757a5a710fc37
Mico46 <tex.mic22@gmail.com>
2026-07-02 00:11:08 +0300

    feat: initialize Firebase app with authentication and Firestore services

--- src/firebase.js
+++ src/firebase.js
@@ -11,6 +11,15 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+const firebaseConfig2 = {
+    apiKey: "AIzaSyAGUO-j3VlYhQM3UDs0FIb1UoVpn3pGxbA",
+    authDomain: "telegrambot-991ab.firebaseapp.com",
+    projectId: "telegrambot-991ab",
+    storageBucket: "telegrambot-991ab.firebasestorage.app",
+    messagingSenderId: "1049608530319",
+    appId: "1:1049608530319:web:4bcf6288fd6813d25103fa",
+    measurementId: "G-CP4VCG1FN0"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
